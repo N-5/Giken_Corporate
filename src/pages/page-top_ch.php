7e37968e6a57c42ebc19c909a742937787617909
N-5 <yfe63590@gmail.com>
2019-05-07 17:32:22 +0900

    add english , chinese page

---
+++ src/pages/page-top_ch.php
@@ -0,0 +1,130 @@
+<?php
+/*
+Template Name: chinese-top
+*/
+?>
+<?php get_header(); ?>
+
+<main id="top">
+  <section class="top-hero">
+    <div class="l-container">
+      <p class="top-hero__heading">技術で世界の流れをつくる。</p>
+      <div class="top-hero__catchcopy">MAKE<br>THE <br class="u-sm_max">WORLD <br class="u-sm_max">FROW<br>WITH<br>TECHNOLOGY</div>
+      <div class="top-hero__inner">
+        <p class="top-hero__lead">
+          自動化技術で、世界中のものづくりに流れをつくる。<br>
+          それは世界を動かして、<br>
+          きっと世の中にも新しい流れをつくる。<br>
+          私たちの仕事は、「流れ」そのものを生み出すことだ。<br>
+          技研は、世界を動かす流れの源になる。
+        </p>
+        <div class="top-hero__btn">
+          <div class="m-link m-link__blueborder"><a href="<?php echo home_url(); ?>/vision/"><span>MORE OUR VISION</span></a></div>
+          <div class="m-link m-link__list"><a href="<?php echo home_url(); ?>/vision/movie/"><span>CONCEPT MOVIE</span></a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="top-service">
+    <div class="l-container">
+      <h2 class="m-top-section__title" data-inview><span class="en">SERVICE</span><span class="jp">事業内容</span></h2>
+      <figure class="top-service__img" data-inview><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/top/top-service-figure.png" alt="自動車産業、アルミ産業、産業機械事業"></figure>
+      <div class="top-service__inner" data-inview>
+        <p class="m-top-section__copy">自動化技術を未来とクロスする、<br>それが私たちの仕事。</p>
+        <p class="u-m">ダミー。装置開発やライン開発で培ってきた数多くの経験および各種のノウハウを活かし、お応えしています。装置開発やライン開発で培ってきた数多くの経験および各種のノウハウを活かします。ダミー。装置開発やライン開発で培ってきた数多くの経験および各種のノウハウを活かし、お応えしています。装置開発。</p>
+        <div class="m-link m-link__border"><a href="<?php echo home_url(); ?>/service/"><span>MORE</span></a></div>
+      </div>
+    </div>
+  </section>
+
+  <section class="top-company">
+    <div class="l-container__large">
+      <div class="top-company__award" data-inview>
+        <div class="image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/top/top-company-award.png" alt="PATENTS and AWARDS"></div>
+        <div class="text">日/米/韓の特許を多数取得<br>表彰、感謝状を<br>多数いただいております</div>
+      </div>
+      <div class="top-company__image" data-inview>
+        <picture>
+          <source media="(max-width:769px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/top/top-company-image_sp.jpg">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/top/top-company-image_pc.jpg" alt="技研外観写真">
+        </picture>
+      </div>
+      <div class="top-company__inner" data-inview>
+        <h2 class="m-top-section__title"><span class="en">COMPANY</span><span class="jp">会社概要</span></h2>
+        <p class="m-top-section__copy">ずっと安心して使い続けられる、<br>良質なものを世界へ。</p>
+        <p class="u-m">ダミー。装置開発やライン開発で培ってきた数多くの経験および各種のノウハウを活かし、お客さまの多お応えしています。</p>
+        <div class="m-link m-link__blueborder"><a href="<?php echo home_url(); ?>/company/"><span>MORE</span></a></div>
+      </div>
+    </div>
+  </section>
+
+  <section class="top-information" data-inview>
+    <div class="top-information__outer">
+      <div class="top-information__recruit">
+        <div class="top-information__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/top/top-recruit__image.jpg" alt="採用情報"></div>
+        <div class="top-information__inner">
+          <h2 class="m-top-section__title"><span class="en">RECRUIT</span><span class="jp">採用情報</span></h2>
+          <h3 class="m-top-section__copy">世界と交わり、<br>次世代を担う。</h3>
+          <p class="u-m">ダミー。装置開発やライン開発で培ってきた数多くの経験および各種のノウハウを活かし、お客さまの多お応えしています。</p>
+          <div class="m-link m-link__border"><a href="<?php echo home_url(); ?>/recruit/"><span>MORE</span></a></div>
+        </div>
+      </div>
+      <div class="top-information__csr">
+        <div class="top-information__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/top/top-csr__image.jpg" alt="社会への取り組み"></div>
+        <div class="top-information__inner">
+          <h2 class="m-top-section__title"><span class="en">CSR</span><span class="jp">社会への取り組み</span></h2>
+          <h3 class="m-top-section__copy">社会と重なり、<br>大切な物を築く。</h3>
+          <p class="u-m">ダミー。装置開発やライン開発で培ってきた数多くの経験および各種のノウハウを活かし、お客さまの多お応えしています。</p>
+          <div class="m-link m-link__border"><a href="<?php echo home_url(); ?>/csr/"><span>MORE</span></a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="top-news">
+    <div class="l-container">
+      <h2 class="m-top-section__title" data-inview><span class="en">NEWS</span><span class="jp">新着情報</span></h2>
+      <div class="top-news-list">
+        <?php
+        $args = array(
+          'post_type' => 'news',
+          'posts_per_page' => 4
+        );
+        $the_query = new WP_Query($args); if($the_query->have_posts()):
+        ?>
+        <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+        <article class="top-news-list__item" data-inview>
+          <a href="<?php the_permalink() ?>">
+            <div class="detail">
+              <time class="time"><?php the_time('Y.m.d') ?></time>
+              <div class="categogy">
+                <?php $custom_post_tag = 'news-category';
+             $custom_post_tag_terms = wp_get_object_terms($post->ID, $custom_post_tag);
+             if(!empty($custom_post_tag_terms)){
+               if(!is_wp_error( $custom_post_tag_terms )){
+                 foreach($custom_post_tag_terms as $term){
+                   $tag_term_link = get_term_link($term->slug, $custom_post_tag);
+                   $tag_term_name = $term->name;
+                   echo $tag_term_name;
+                 }
+               }
+             }
+                ?>
+              </div>
+            </div>
+            <div class="title"><?php the_title() ?></div>
+          </a>
+        </article>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+        <?php else: ?>
+        <p>ニュースが投稿されていません。</p>
+        <?php endif; ?>
+        <div class="m-link m-link__list" data-inview><a href="<?php echo home_url(); ?>/news/"><span>VIEW LIST</span></a></div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
