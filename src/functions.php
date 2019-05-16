ce8bf59c69d78d2f842178bb6b3590278eedfe37
N-5 <yfe63590@gmail.com>
2019-05-16 11:36:00 +0900

    delete double funciton code

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,5 @@
 <?php
 
-//remove Emojis
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-
 //remove
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'rsd_link');
